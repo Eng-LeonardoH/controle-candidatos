b81bebf99d4f946cc31950c0cb1cb279651cb360
Eng-LeonardoH <leonardoham321@gmail.com>
2024-09-04 10:01:07 -0300

    Analise de candidatos

--- src/candidatura/ProcessoSeletivo.java
+++ src/candidatura/ProcessoSeletivo.java
@@ -1,4 +1,21 @@
 package candidatura;
 
 public class ProcessoSeletivo {
+     public static void main(String[] args) {
+          System.out.println("Processo seletivo");
+          analisarCandidato(1900);
+          analisarCandidato(2000);
+          analisarCandidato(2200);
+     }
+
+     static void analisarCandidato(double salarioPretendido) {
+          double salarioBase = 2000;
+          if (salarioBase > salarioPretendido) {
+               System.out.println("LIGAR PARA O CANDIDATO");
+          } else if (salarioBase == salarioPretendido) {
+               System.out.println("LIGAR PARA O CANDIDATO COM CONTRA PROPOSTA");
+          } else {
+               System.out.println("AGUARDANDO O RESULTADO DOS DEMAIS CANDIDATOS");
+          }
+     }
 }
